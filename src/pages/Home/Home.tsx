903fe2a41c0dfbe905ad4356a5bc94e2f98b305a
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-06-24 15:50:31 +0200

    sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 function Home() {
   return (
     <>
-      {/* <section className="">Welcome</section>
+      <section className="">Welcome</section>
       <section className="">Shop</section>
       <section className="">Advertisement</section>
       <section className="">BlogPOsts</section>
       <section> Subscription</section>
-      <section> Contact</section> */}
+      <section> Contact</section>
     </>
   );
 }
